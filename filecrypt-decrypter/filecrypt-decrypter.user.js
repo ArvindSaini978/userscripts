e0c621e0a3998ae01e589518c50ef702201f0261
Arvind Saini <123250522+ArvindSaini978@users.noreply.github.com>
2026-09-26 08:57:18 +0530

    Update filecrypt-decrypter.user.js version to 2.0.1

--- filecrypt-decrypter/filecrypt-decrypter.user.js
+++ filecrypt-decrypter/filecrypt-decrypter.user.js
@@ -2,7 +2,7 @@
 // @name         Filecrypt Instant Decrypter & Auto-Resolver (CNL + High Speed)
 // @namespace    https://github.com/ArvindSaini978/userscripts/
 // @description  Instantly decrypts and resolves Filecrypt containers. Features AES Click'n'Load decryption, multi-worker queues, auto-retry for dead/slow links, and 1-click batch copy.
-// @version      2.0.0
+// @version      2.0.1
 // @author       ArvindSaini978
 // @license      MIT
 // @match        *://*.filecrypt.cc/*
